013c9e4843c0f38e27bd202c7fa40efa138ab2a4
Dale S Hendrickson <dale@dshendrickson.com>
2016-10-30 07:52:27 -0600

    Added constructor to component and created new stateless component for title bar

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -4,11 +4,29 @@ import { pick, map, extend, filter} from 'lodash';
 import moment from 'moment';
 
 export default class Application extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      numberToGuess: '',
+      numberGuessed: '',
+      numberRangeLow: '',
+      numberRangeHigh: '',
+      guessFeedbackMessage: ''
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Number Guesser</h1>
-      </div>
+      <TitleBar />
     )
   }
+}
+
+const TitleBar = () => {
+  return (
+    <div className='title-bar'>
+      <h1>Number Guesser with React</h1>
+    </div>
+  )
 }
