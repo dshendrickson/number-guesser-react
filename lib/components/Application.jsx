003f995f0999143c200c7e835c14469493bc5962
Dale S Hendrickson <dale@dshendrickson.com>
2016-10-30 20:41:10 -0600

    Fix random number to only generate on load

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import { pick, map, extend, filter} from 'lodash';
 import moment from 'moment';
 
@@ -11,17 +12,19 @@ import Reset from './Reset';
 
 export default class Application extends Component {
 
+
   constructor() {
     super();
     this.state = {
       numberToGuess: '',
-      numberGuessed: '',
+      numberGuessed: 'Best Guess!',
       numberRangeLow: 1,
       numberRangeHigh: 100,
       guessFeedbackMessage: 'Enter your guess below:',
       previousGuessMessage: 'Waiting for your best guess!',
       numberOfWins: 0
     };
+    this.generateNumberToGuess();
   }
 
   generateNumberToGuess() {
@@ -36,8 +39,11 @@ export default class Application extends Component {
     this.setState({numberRangeLow: entered.target.value})
   }
 
+  updateNumberGuessed(entered) {
+    this.setState({numberGuessed: entered.target.value})
+  }
+
   render() {
-    this.generateNumberToGuess();
     return (
       <div>
         <TitleBar />
@@ -46,9 +52,13 @@ export default class Application extends Component {
                     updateRangeValueLow={this.updateRangeValueLow.bind(this)}
                     updateRangeValueHigh={this.updateRangeValueHigh.bind(this)} />
         <GuessResult value={this.state} />
-        <Guess />
+        <Guess numberGuessed={this.state.numberGuessed}
+              updateNumberGuessed={this.updateNumberGuessed.bind(this)}/>
         <Reset />
       </div>
     )
   }
+
+
+
 }
--- lib/components/Guess.jsx
+++ lib/components/Guess.jsx
@@ -8,9 +8,10 @@ export default class Guess extends Component {
       <div id='guess'>
         <input id='guess__input'
               name='numberGuessed'
-              placeholder='Best Guess!'
+              placeholder={this.props.numberGuessed}
               aria-label='enter your best guess' />
         <button id='guess__submit'
+                onClick={this.props.updateNumberGuessed}
                 aria-label='submit your guess button'>submit</button>
         <button id='guess__clear'
                 aria-label='clear your guess button'>clear</button>
