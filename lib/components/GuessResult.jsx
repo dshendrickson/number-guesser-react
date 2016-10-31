0457e5aed52c5aed58294e52b2035fc00509a11b
Dale S Hendrickson <dale@dshendrickson.com>
2016-10-30 18:56:11 -0600

    CSS id for guess value

--- lib/components/GuessResult.jsx
+++ lib/components/GuessResult.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 
 export default class GuessResult extends Component {
   render() {
+
+    let numberToGuess = this.props.value.numberToGuess
+
     return (
-      <div className='guess-result'>
-        <h3 >{this.props.value.guessFeedbackMessage}</h3>
-        <p>0</p>
+      <div id='guess-result'>
+        <h3>{this.props.value.guessFeedbackMessage}</h3>
+        <h2 id='guess-result__value'>{parseInt(numberToGuess)}</h2>
         <h3>{this.props.value.previousGuessMessage}</h3>
       </div>
     )
